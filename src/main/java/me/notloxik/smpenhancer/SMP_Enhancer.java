0c2ddeb6e0dcff23987c1779231545901e23a788
Loxik <hello@loxik.dev>
2023-09-19 18:18:52 +0200

    No way I forgot to update this.

--- src/main/java/me/notloxik/smpenhancer/SMP_Enhancer.java
+++ src/main/java/me/notloxik/smpenhancer/SMP_Enhancer.java
@@ -1,17 +1,34 @@
 package me.notloxik.smpenhancer;
 
+import me.notloxik.smpenhancer.commands.*;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
 
 public final class SMP_Enhancer extends JavaPlugin {
 
+    private static SMP_Enhancer instance;
+    private TpaManager tpaManager;
+
+    @Override
+    public void onLoad() {
+        this.tpaManager = new TpaManager();
+    }
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
+        getCommand("sit").setExecutor(new SitCommand());
+        getCommand("tpa").setExecutor(new TpaCommand(tpaManager));
+        getCommand("tpaccept").setExecutor(new TpacceptCommand(tpaManager));
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public static SMP_Enhancer getInstance() {
+        return instance;
     }
+
+
+
 }
